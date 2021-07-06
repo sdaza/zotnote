01428e96fe962d792fcbdda14f419bba3b2b95ad
sdaza <sebastian.daza@gmail.com>
2021-07-06 10:10:39 +0200

    minor updates

--- zotnote/functions.py
+++ zotnote/functions.py
@@ -43,36 +43,40 @@ def exportNotes(library_type="group", collection=None, library_id=None,
             notes = notes.append({"notes": ""}, ignore_index=True)
         notes = notes.loc[[(len(x) > 0 and separator in x) for x in notes["notes"]]]
 
-    citations = pandas.DataFrame(columns=["citation"])
-    for parent in notes["parent"]:
-        citations = citations.append(
-            {
-                "citation": zot.item(
-                    parent,
-                    content="citation",
-                    format="keys",
-                    style="taylor-and-francis-council-of-science-editors-author-date",
-                ),
-            "parent": parent,
-            "tags": zot.item_tags(parent), 
-            "title": zot.item(parent)["data"]["title"] 
-            },
-            ignore_index=True,
-        )
+    if (len(notes.index) > 0):
+        citations = pandas.DataFrame(columns=["citation"])
+        for parent in notes["parent"]:
+            citations = citations.append(
+                {
+                    "citation": zot.item(
+                        parent,
+                        content="citation",
+                        format="keys",
+                        style="taylor-and-francis-council-of-science-editors-author-date",
+                    ),
+                "parent": parent,
+                "tags": zot.item_tags(parent), 
+                "title": zot.item(parent)["data"]["title"] 
+                },
+                ignore_index=True,
+            )
     
-    res = citations.set_index("parent").join(notes.set_index("parent"))
-    res["notes"] = [re.sub("<.*?>", "", str(x)) for x in res["notes"]]
-    notes = extractNotes(res["notes"], separator=separator)
+        res = citations.set_index("parent").join(notes.set_index("parent"))
+        res["notes"] = [re.sub("<.*?>", "", str(x)) for x in res["notes"]]
+        notes = extractNotes(res["notes"], separator=separator)
 
-    ref = citations.set_index("parent")
-    ref["citation"] = [re.sub("\[|\]|\(|\)|'", "", str(x)) for x in ref["citation"]]
-    ref["citation"] = [re.sub("<.*?>", "", str(x)) for x in ref["citation"]]
-    ref["title"] = [re.sub("\['|'\]", "", str(x)) for x in ref["title"]]
-    ref["tags"] = [re.sub("\[|\]|']", "", str(x)) for x in ref["tags"]]
-    ref["tags"] = [re.sub("'", "", str(x)) for x in ref["tags"]]
-    ref = pandas.DataFrame(ref)
-    
-    ref.merge(notes, left_on='parent', right_on='parent') \
-        .rename(columns = {"parent" : "id"}, inplace=False) \
-        .drop_duplicates() \
-        .to_csv(file, index=False)
+        ref = citations.set_index("parent")
+        ref["citation"] = [re.sub("\[|\]|\(|\)|'", "", str(x)) for x in ref["citation"]]
+        ref["citation"] = [re.sub("<.*?>", "", str(x)) for x in ref["citation"]]
+        ref["title"] = [re.sub("\['|'\]", "", str(x)) for x in ref["title"]]
+        ref["tags"] = [re.sub("\[|\]|']", "", str(x)) for x in ref["tags"]]
+        ref["tags"] = [re.sub("'", "", str(x)) for x in ref["tags"]]
+        ref = pandas.DataFrame(ref)
+        
+        print("Notes saved in", file)
+        ref.merge(notes, left_on='parent', right_on='parent') \
+            .rename(columns = {"parent" : "id"}, inplace=False) \
+            .drop_duplicates() \
+            .to_csv(file, index=False)
+    else: 
+        print("No notes!")
